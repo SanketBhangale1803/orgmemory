f3ea10cbce07bf2d4fe3aca3d0fb5d1333ca1c9d
Sanket Bhangale <124848504+SanketBhangale1803@users.noreply.github.com>
2026-09-01 23:31:30 -0500

    Perform cleanup

--- artifacts/webmcp-demo/remotion/src/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { loadFont } from "@remotion/fonts";
-import { staticFile } from "remotion";
-
-await loadFont({
-  family: "OrgMemory Sans",
-  url: staticFile("SFNS.ttf"),
-  format: "truetype",
-  display: "block",
-});
